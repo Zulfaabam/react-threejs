1e66ad92ed0bdad75b4787c1b6de5e2ba1b9941d
Zulfaabam <zulfafatahakbar@gmail.com>
2024-12-01 20:44:56 +0700

    create geometry and sparkles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import './App.css';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Sparkles } from '@react-three/drei';
+import { useRef } from 'react';
+
+const RotatingCube = () => {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.y += 0.01;
+      meshRef.current.rotation.x += 0.01;
+    }
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <cylinderGeometry args={[1, 1, 1]} />
+      <meshLambertMaterial color='#468585' emissive='#468585' />
+      <Sparkles
+        count={100}
+        scale={1}
+        size={6}
+        speed={0.002}
+        noise={0.2}
+        color='orange'
+      />
+    </mesh>
+  );
+};
 
 function App() {
   return (
@@ -16,6 +43,7 @@ function App() {
       <OrbitControls enablePan enableZoom enableRotate />
       <directionalLight position={[1, 1, 1]} intensity={10} color={0x9cdba6} />
       <color attach='background' args={['#f0f0f0']} />
+      <RotatingCube />
     </Canvas>
   );
 }
